68e76cdefe23a1d066575b344c57b2b159966bd1
Kimhyelyn <44598936+merry-hyelyn@users.noreply.github.com>
2020-08-07 22:47:23 +0900

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,9 +1,8 @@
 from boards.models import Board
 from posts.models import Post
-from django.views.generic import TemplateView, DeleteView
+from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404
 
 
 class IndexView(TemplateView):
@@ -13,21 +12,17 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['board_list'] = Board.objects.all()
+        path = ""
 
+        for v in kwargs.values():
+            path = v
+
+        posts = Post.objects.filter(board__path=path)
         path = kwargs.get('path', '')
         posts_list = Post.objects.filter(board__path=path)
         pagination = Paginator(posts_list, 6)
-        context['pagination'] = pagination
-
         page = self.request.GET.get('page')
+        print('kwargs', kwargs, pagination)
         posts = pagination.get_page(page)
         context['post_list'] = posts
         return context
-
-
-class BoardDelete(DeleteView):
-    model = Board
-    success_url = reverse_lazy("home")
-
-    def get_object(self):
-        return get_object_or_404(Board, pk=self.kwargs.get("pk"))
